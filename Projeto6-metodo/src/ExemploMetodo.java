72caadc8cb0eb0523c73c7a235093a09c6f2943a
Ronaldo Faustino <ohnirucse@msn.com>
2018-06-25 22:46:53 -0300

    Aula 7

---
+++ Projeto6-metodo/src/ExemploMetodo.java
@@ -0,0 +1,20 @@
+import java.nio.charset.MalformedInputException;
+
+public class ExemploMetodo {
+	
+	static void exibirNome(String nome, int quantidade){
+		for (int i = 0; i< quantidade; i++){
+			System.out.println(nome + i);
+		}
+	}
+	public static void main(String[] args){
+		System.out.println("Exibindo o nome");
+		exibirNome("Sandro", 4);
+		
+		System.out.println("Exibindo o nome novamente");
+		exibirNome("Maria", 7);
+	}
+}
+
+
+
